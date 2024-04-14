fc41660eecb33518f6a730c1b9c0ae2abf44ed37
Qzimyion <samyakchauhan676@gmail.com>
2024-04-14 23:49:58 +0530

    Set up for update 1.0.3

---
+++ src/main/java/com/qzimyion/bucketem/mixin/EntityMixins/LivingEntityMixin.java
@@ -0,0 +1,46 @@
+package com.qzimyion.bucketem.mixin.EntityMixins;
+
+import com.qzimyion.bucketem.potions.StatusEffects.ModStatusEffectsRegistry;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@SuppressWarnings("SameParameterValue")
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+    @Shadow public abstract boolean hasStatusEffect(StatusEffect effect);
+
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;hasNoGravity()Z"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isInLava()Z"), to = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isFallFlying()Z")))
+    private void modifyLavaSpeed(Vec3d movementInput, CallbackInfo ci){
+        if (this.hasStatusEffect(ModStatusEffectsRegistry.BLISTERED_VISION)){
+            this.setVelocity(bucketEm$LavaVelocity(movementInput, 0.14f, this.getYaw()));
+        }
+    }
+
+    @Unique
+    private static Vec3d bucketEm$LavaVelocity(Vec3d movementInput, float speed, float yaw) {
+        double d = movementInput.lengthSquared();
+        if (d < 1.0E-7) {
+            return Vec3d.ZERO;
+        }
+        Vec3d vec3d = (d > 1.0 ? movementInput.normalize() : movementInput).multiply(speed);
+        float f = MathHelper.sin(yaw * ((float)Math.PI / 180));
+        float g = MathHelper.cos(yaw * ((float)Math.PI / 180));
+        return new Vec3d(vec3d.x * (double)g - vec3d.z * (double)f, vec3d.y, vec3d.z * (double)g + vec3d.x * (double)f);
+    }
+}
