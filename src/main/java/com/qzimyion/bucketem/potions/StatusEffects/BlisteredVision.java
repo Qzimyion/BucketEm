9311325e1164337bf1c3115dd3b9b5123972d4f9
Qzimyion <samyakchauhan676@gmail.com>
2024-04-14 23:49:36 +0530

    Set up for update 1.0.3

--- src/main/java/com/qzimyion/bucketem/potions/StatusEffects/BlisteredVision.java
+++ src/main/java/com/qzimyion/bucketem/potions/StatusEffects/BlisteredVision.java
@@ -1,12 +1,25 @@
 package com.qzimyion.bucketem.potions.StatusEffects;
 
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class BlisteredVision extends StatusEffect {
 
     public BlisteredVision(){
         super(StatusEffectCategory.BENEFICIAL, 0x661348);
+    }
+
+    @Override
+    public void onApplied(LivingEntity entity, int amplifier) {
+        EntityPose swimming = EntityPose.SWIMMING;
+        if (entity instanceof PlayerEntity){
+            if (entity.isInLava()) {
+                entity.setPose(swimming);
+            }
+        }
     }
 
     @Override
