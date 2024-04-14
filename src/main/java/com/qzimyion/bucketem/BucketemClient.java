ca8c5288096aa9ce2201e227333032286349298b
Qzimyion <samyakchauhan676@gmail.com>
2024-04-14 23:35:25 +0530

    Set up for update 1.0.3

--- src/main/java/com/qzimyion/bucketem/BucketemClient.java
+++ src/main/java/com/qzimyion/bucketem/BucketemClient.java
@@ -1,12 +1,8 @@
 package com.qzimyion.bucketem;
 
-
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
 
 public class BucketemClient implements ClientModInitializer {
-
-
     public void onInitializeClient(){
 
     }
